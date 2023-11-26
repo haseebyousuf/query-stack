d774ce32474a280a3307986e6608e516123fa55f
Haseeb Yousuf <haseeb@Haseebs-MacBook-Air.local>
2023-11-26 20:44:05 +0530

    modify layout and add assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
+import { Inter, Space_Grotesk } from 'next/font/google';
 import './globals.css';
+import { ClerkProvider } from '@clerk/nextjs';
 
-const inter = Inter({ subsets: ['latin'] });
-
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter',
+});
+const spaceGrotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-spaceGrotesk',
+});
 export const metadata: Metadata = {
   title: 'Query Stack',
-  description: 'AI StackOverflow',
+  description: `A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world Explore topics in web development, mobile app development, algorithms, data structures, and more.`,
+  icons: {
+    icon: 'assets/images/site-logo.svg',
+  },
 };
 export default function RootLayout({
   children,
@@ -15,8 +27,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang='en'>
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient hover:text-primary-500',
+        },
+      }}
+    >
+      <html lang='en'>
+        <body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
