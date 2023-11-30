f57fc1ac3234e8e907bd26f6b575b811e413dd3e
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-11-30 23:08:36 +0530

    Refactor ask-question page component

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import { auth } from '@clerk/nextjs';
-import { redirect } from 'next/navigation';
+// import { auth } from '@clerk/nextjs';
+// import { redirect } from 'next/navigation';
+import QuestionForm from '@/components/forms/QuestionForm';
 const AskQuestion = () => {
-  const { userId } = auth();
+  // const { userId } = auth();
 
-  if (!userId) redirect('/sign-in');
-  return <div>AskQuestion</div>;
+  // if (!userId) redirect('/sign-in');
+  return (
+    <div>
+      <h1 className='h1-bold text-dark100_light900'>Ask a Question</h1>
+      <div className='mt-9'>
+        <QuestionForm />
+      </div>
+    </div>
+  );
 };
 
 export default AskQuestion;
