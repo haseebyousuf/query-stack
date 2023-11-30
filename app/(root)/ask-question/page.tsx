21530de5e434e2e7da09d930d2984f2130d3b763
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-11-30 18:10:29 +0530

    Add authentication check to AskQuestion page

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 const AskQuestion = () => {
+  const { userId } = auth();
+
+  if (!userId) redirect('/sign-in');
   return <div>AskQuestion</div>;
 };
 
