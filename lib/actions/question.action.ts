bff9cd1af6b1069a5d8d22e2429f1b3ff12829f3
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-12-04 19:53:38 +0530

    Refactor createQuestion function and add getQuestions function

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -2,8 +2,24 @@
 import { connectToDatabase } from '../mongoose';
 import Question from '@/database/question.model';
 import Tag from '@/database/tag.model';
+import { CreateQuestionParams, GetQuestionsParams } from './shared.types';
+import User from '@/database/user.model';
+import { revalidatePath } from 'next/cache';
 
-export async function createQuestion(params: any) {
+export async function getQuestions(params: GetQuestionsParams) {
+  try {
+    connectToDatabase();
+    const questions = await Question.find({})
+      .populate({ path: 'tags', model: Tag })
+      .populate({ path: 'author', model: User })
+      .sort({ createdAt: -1 });
+    return { questions };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+export async function createQuestion(params: CreateQuestionParams) {
   try {
     connectToDatabase();
     const { title, content, tags, author, path } = params;
@@ -28,6 +44,8 @@ export async function createQuestion(params: any) {
     await Question.findByIdAndUpdate(question._id, {
       $push: { tags: { $each: tagDocuments } },
     });
+
+    revalidatePath(path);
   } catch (err) {
     console.log('first');
   }
