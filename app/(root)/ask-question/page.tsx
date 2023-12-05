464bcb91e42abf800b6d794f4b61d3e01f3b73df
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-12-06 00:33:49 +0530

    Fix import statement and remove console.log

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
-// import { auth } from '@clerk/nextjs';
+import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
 import QuestionForm from '@/components/forms/QuestionForm';
 import { getUserById } from '@/lib/actions/user.action';
 const AskQuestion = async () => {
-  // const { userId } = auth();
-  const userId = 'CL123';
+  const { userId } = auth();
   if (!userId) redirect('/sign-in');
   const mongoUser = await getUserById({ userId });
-  console.log(mongoUser);
   return (
     <div>
       <h1 className='h1-bold text-dark100_light900'>Ask a Question</h1>
