0282924e1c909f9b0a292faec88d9344790992aa
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-12-06 20:05:47 +0530

    Update Filter component styles

--- components/shared/Filter.tsx
+++ components/shared/Filter.tsx
@@ -28,10 +28,14 @@ const Filter = ({ filters, otherClasses, containerClasses }: FilterProps) => {
             <SelectValue placeholder='Filter' />
           </div>
         </SelectTrigger>
-        <SelectContent>
+        <SelectContent className='text-dark500_light700 small-regular border-none bg-light-900 dark:bg-dark-300'>
           <SelectGroup>
             {filters.map((filter) => (
-              <SelectItem key={filter.value} value={filter.value}>
+              <SelectItem
+                key={filter.value}
+                value={filter.value}
+                className='cursor-pointer focus:bg-light-800 dark:focus:bg-dark-400'
+              >
                 {filter.name}
               </SelectItem>
             ))}
