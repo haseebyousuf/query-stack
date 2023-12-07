680dd5aaddd11c8d9e117c3e8662c3e2c3376ff5
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-12-07 19:47:13 +0530

    Add getQuestionById function to question.action.ts

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -2,7 +2,11 @@
 import { connectToDatabase } from '../mongoose';
 import Question from '@/database/question.model';
 import Tag from '@/database/tag.model';
-import { CreateQuestionParams, GetQuestionsParams } from './shared.types';
+import {
+  CreateQuestionParams,
+  GetQuestionByIdParams,
+  GetQuestionsParams,
+} from './shared.types';
 import User from '@/database/user.model';
 import { revalidatePath } from 'next/cache';
 
@@ -19,6 +23,24 @@ export async function getQuestions(params: GetQuestionsParams) {
     throw error;
   }
 }
+export async function getQuestionById(params: GetQuestionByIdParams) {
+  try {
+    connectToDatabase();
+    const { questionId } = params;
+    const question = await Question.findById(questionId)
+      .populate({ path: 'tags', model: Tag, select: '_id name' })
+      .populate({
+        path: 'author',
+        model: User,
+        select: '_id clerkId name picture',
+      });
+    return question;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
 export async function createQuestion(params: CreateQuestionParams) {
   try {
     connectToDatabase();
