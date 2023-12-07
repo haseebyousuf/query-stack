a0a4a4a6025f8ff5e3104f5214b2d4f831d27d7c
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-12-07 18:44:50 +0530

    Add getAllTags function to retrieve all tags

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -1,7 +1,8 @@
 'use server';
 import User from '@/database/user.model';
 import { connectToDatabase } from '../mongoose';
-import { GetTopInteractedTagsParams } from './shared.types';
+import { GetAllTagsParams, GetTopInteractedTagsParams } from './shared.types';
+import Tag from '@/database/tag.model';
 
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
@@ -14,6 +15,17 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
       { _id: '1', name: 'next.js' },
       { _id: '2', name: 'React' },
     ];
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function getAllTags(params: GetAllTagsParams) {
+  try {
+    connectToDatabase();
+    const tags = await Tag.find({});
+    return { tags };
   } catch (error) {
     console.log(error);
     throw error;
