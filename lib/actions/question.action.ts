36bdc49951a55e1aea1576f6d586bb0ef93bb4ff
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-11 20:46:08 +0530

    Add deleteQuestion and editQuestion functions

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -4,12 +4,16 @@ import Question from '@/database/question.model';
 import Tag from '@/database/tag.model';
 import {
   CreateQuestionParams,
+  DeleteQuestionParams,
+  EditQuestionParams,
   GetQuestionByIdParams,
   GetQuestionsParams,
   QuestionVoteParams,
 } from './shared.types';
 import User from '@/database/user.model';
 import { revalidatePath } from 'next/cache';
+import Answer from '@/database/answer.model';
+import Interaction from '@/database/interaction.model';
 
 export async function getQuestions(params: GetQuestionsParams) {
   try {
@@ -130,6 +134,45 @@ export async function downvoteQuestion(params: QuestionVoteParams) {
       throw new Error('Question not found');
     }
     // increment author reputation
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function deleteQuestion(params: DeleteQuestionParams) {
+  try {
+    connectToDatabase();
+    const { questionId, path } = params;
+    await Question.deleteOne({ _id: questionId });
+    await Answer.deleteMany({ question: questionId });
+    await Interaction.deleteMany({ question: questionId });
+    await Tag.updateMany(
+      { questions: questionId },
+      { $pull: { questions: questionId } }
+    );
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function editQuestion(params: EditQuestionParams) {
+  try {
+    connectToDatabase();
+    const { questionId, title, content, path } = params;
+
+    const question = await Question.findById(questionId).populate('tags');
+
+    if (!question) {
+      throw new Error('Question not found');
+    }
+    question.title = title;
+    question.content = content;
+    await question.save();
     revalidatePath(path);
   } catch (error) {
     console.log(error);
