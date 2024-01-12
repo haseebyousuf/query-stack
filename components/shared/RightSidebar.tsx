cb90e61157161ada1a233a1bb3fa6e9f3c71c388
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-12 16:33:26 +0530

    Refactor RightSidebar component to fetch hot questions and popular tags dynamically

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -2,26 +2,12 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import RenderTag from './RenderTag';
-const hotQuestions = [
-  {
-    _id: 1,
-    title:
-      'Best practices for data fetching in a Next.js application with Server-Side Rendering (SSR)?',
-  },
-  { _id: 2, title: 'Is it only me or the font is bolder than necessary' },
-  { _id: 3, title: 'Can I get the course for free?' },
-  { _id: 4, title: 'Redux Toolkit Not Updating State as Expected' },
-  { _id: 5, title: 'Async/Await Function Not Handling Errors Properly' },
-];
+import { getHotQuestions } from '@/lib/actions/question.action';
+import { getPopularTags } from '@/lib/actions/tag.action';
 
-const popularTags = [
-  { _id: 1, name: 'javascript', totalQuestions: 24 },
-  { _id: 2, name: 'react', totalQuestions: 10 },
-  { _id: 3, name: 'typescript', totalQuestions: 22 },
-  { _id: 4, name: 'nextjs', totalQuestions: 43 },
-  { _id: 5, name: 'redux', totalQuestions: 99 },
-];
-const RightSidebar = () => {
+const RightSidebar = async () => {
+  const hotQuestions = await getHotQuestions();
+  const popularTags = await getPopularTags();
   return (
     <section className='background-light900_dark200 light-border custom-scrollbar sticky right-0 top-0 flex h-screen w-[350px] flex-col  overflow-y-auto border-l p-6 pt-36 shadow-light-300 dark:shadow-none max-xl:hidden'>
       <div>
@@ -29,7 +15,7 @@ const RightSidebar = () => {
         <div className='mt-7 flex w-full flex-col gap-[30px]'>
           {hotQuestions.map((question) => (
             <Link
-              href={`/questions/${question._id}`}
+              href={`/question/${question._id}`}
               key={question._id}
               className='flex cursor-pointer items-center justify-between gap-7'
             >
@@ -56,7 +42,7 @@ const RightSidebar = () => {
               key={tag._id}
               _id={tag._id}
               name={tag.name}
-              totalQuestions={tag.totalQuestions}
+              totalQuestions={tag.numberOfQuestions}
               showCount={true}
             />
           ))}
