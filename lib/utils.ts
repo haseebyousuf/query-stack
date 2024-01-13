0652ffe6cc9d42ce42bb0bf5e339c0dbdb9cadff
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-13 19:58:14 +0530

    Add URL query parameter manipulation functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from 'clsx';
+import qs from 'query-string';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
@@ -62,3 +63,39 @@ export function getJoinedDate(date: Date): string {
 
   return joinedDate;
 }
+
+type UrlQueryParams = {
+  params: string;
+  key: string;
+  value: string | null;
+};
+export const formUrlQuery = ({ params, key, value }: UrlQueryParams) => {
+  const currentUrl = qs.parse(params);
+  currentUrl[key] = value;
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: currentUrl,
+    },
+    { skipNull: true }
+  );
+};
+
+type RemoveUrlQueryParams = {
+  params: string;
+  keysToRemove: string[];
+};
+export const removeKeysFromQuery = ({
+  params,
+  keysToRemove,
+}: RemoveUrlQueryParams) => {
+  const currentUrl = qs.parse(params);
+  keysToRemove.forEach((key) => delete currentUrl[key]);
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: currentUrl,
+    },
+    { skipNull: true }
+  );
+};
