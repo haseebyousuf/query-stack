547391f9b255a7c711d82e460837ae55bc86f2c7
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-13 19:58:50 +0530

    Refactor LocalSearch component to include search functionality

--- components/shared/search/LocalSearch.tsx
+++ components/shared/search/LocalSearch.tsx
@@ -1,7 +1,9 @@
 'use client';
 import { Input } from '@/components/ui/input';
+import { formUrlQuery, removeKeysFromQuery } from '@/lib/utils';
 import Image from 'next/image';
-import React from 'react';
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
 
 type CustomInputProps = {
   route: string;
@@ -18,6 +20,33 @@ const LocalSearch = ({
   placeholder,
   otherClasses,
 }: CustomInputProps) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const query = searchParams.get('q');
+  const [search, setSearch] = useState(query || '');
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      if (search) {
+        const newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: 'q',
+          value: search,
+        });
+        router.push(newUrl, { scroll: false });
+      } else {
+        if (pathname === route) {
+          const newUrl = removeKeysFromQuery({
+            params: searchParams.toString(),
+            keysToRemove: ['q'],
+          });
+          router.push(newUrl, { scroll: false });
+        }
+      }
+    }, 300);
+    return () => clearTimeout(delayDebounceFn);
+  }, [search, route, pathname, router, searchParams, query]);
   return (
     <div
       className={`background-light800_darkgradient relative flex min-h-[56px] grow items-center gap-4 rounded-[10px] px-4 ${otherClasses}`}
@@ -35,8 +64,10 @@ const LocalSearch = ({
       <Input
         type='text'
         placeholder={placeholder}
-        value=''
-        onChange={() => {}}
+        value={search}
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
         className='paragraph-regular no-focus placeholder text-dark400_light700 background-light800_darkgradient border-none shadow-none outline-none'
       />
       {iconPosition === 'right' && (
