50d19de54276c51628103b28a77237a55e29ffa8
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-13 19:58:22 +0530

    Refactor Home component to include search query in getQuestions() call

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -6,11 +6,14 @@ import LocalSearch from '@/components/shared/search/LocalSearch';
 import { Button } from '@/components/ui/button';
 import { HomePageFilters } from '@/constants/filters';
 import { getQuestions } from '@/lib/actions/question.action';
+import { SearchParamsProps } from '@/types';
 import Link from 'next/link';
 import React from 'react';
 
-const Home = async () => {
-  const result = await getQuestions({});
+const Home = async ({ searchParams }: SearchParamsProps) => {
+  const result = await getQuestions({
+    searchQuery: searchParams.q,
+  });
   return (
     <>
       <div className='flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center'>
