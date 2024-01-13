8a06fe68e9c9d35ddce9bc34975cef4a6f0ca863
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-13 23:36:45 +0530

    Update AllAnswers component props types

--- components/shared/AllAnswers.tsx
+++ components/shared/AllAnswers.tsx
@@ -11,8 +11,8 @@ type AllAnswersProps = {
   questionId: string;
   userId: string;
   totalAnswers: number;
-  page?: number;
-  filter?: number;
+  page?: string;
+  filter?: string;
 };
 
 const AllAnswers = async ({
@@ -22,7 +22,11 @@ const AllAnswers = async ({
   page,
   filter,
 }: AllAnswersProps) => {
-  const result = await getAnswers({ questionId });
+  const result = await getAnswers({
+    questionId,
+    page: page ? +page : 1,
+    sortBy: filter,
+  });
   return (
     <div className='mt-11'>
       <div className='flex items-center justify-between'>
