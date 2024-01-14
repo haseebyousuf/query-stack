072f55f84c29b7f4f02d02fda4e6c882d4107d9e
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-14 21:02:51 +0530

    Add pagination to getAnswers function

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -36,7 +36,9 @@ export async function getAnswers(params: GetAnswersParams) {
   try {
     connectToDatabase();
 
-    const { questionId, sortBy } = params;
+    const { questionId, sortBy, page = 1, pageSize = 10 } = params;
+    const skipAmount = (page - 1) * pageSize;
+
     let sortOptions = {};
 
     switch (sortBy) {
@@ -55,9 +57,15 @@ export async function getAnswers(params: GetAnswersParams) {
     }
     const answers = await Answer.find({ question: questionId })
       .populate('author', '_id clerkId name picture')
+      .skip(skipAmount)
+      .limit(pageSize)
       .sort(sortOptions);
 
-    return { answers };
+    const totalAnswers = await Answer.countDocuments({ question: questionId });
+
+    const isNext = totalAnswers > skipAmount + answers.length;
+
+    return { answers, isNext };
   } catch (error) {
     console.log(error);
     throw error;
