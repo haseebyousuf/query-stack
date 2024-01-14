8dee985a78a98285a9a673d3deb666d73632ec4d
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-14 21:03:40 +0530

    Add Pagination component to QuestionTab

--- components/shared/QuestionTab.tsx
+++ components/shared/QuestionTab.tsx
@@ -2,6 +2,7 @@ import { getUserQuestions } from '@/lib/actions/user.action';
 import { SearchParamsProps } from '@/types';
 import React from 'react';
 import QuestionCard from '../cards/QuestionCard';
+import Pagination from './Pagination';
 
 interface QuestionTabProps extends SearchParamsProps {
   userId: string;
@@ -13,7 +14,10 @@ const QuestionTab = async ({
   userId,
   clerkId,
 }: QuestionTabProps) => {
-  const result = await getUserQuestions({ userId, page: 1 });
+  const result = await getUserQuestions({
+    userId,
+    page: searchParams.page ? +searchParams.page : 1,
+  });
   return (
     <>
       {result.questions.map((question: any) => (
@@ -30,6 +34,12 @@ const QuestionTab = async ({
           createdAt={question.createdAt}
         />
       ))}
+      <div className='mt-10'>
+        <Pagination
+          pageNumber={searchParams?.page ? +searchParams.page : 1}
+          isNext={result?.isNext}
+        />
+      </div>
     </>
   );
 };
