e60d2019147159f74c58c9627d8a10848136b931
Haseeb Yousuf <haseebyousuff@gmail.com>
2024-01-16 21:50:59 +0530

    Add jobs page component

---
+++ app/(root)/jobs/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const page = () => {
+  return (
+    <div className='flex h-[50vh] items-center justify-center'>
+      <h1 className='font-spaceGrotesk text-6xl font-bold text-dark-100 dark:text-light-900'>
+        Coming{` `} <span className='text-primary-500'>Soon!</span>
+      </h1>
+    </div>
+  );
+};
+
+export default page;
